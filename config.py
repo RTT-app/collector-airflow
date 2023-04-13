4bce2125f536b57a36f4a9b5c9ac199c1412c9ce
jordaoa <andre.nascimento@ccc.ufcg.edu.br>
2023-04-12 21:45:12 -0300

    add: config files

---
+++ config.py
@@ -0,0 +1,10 @@
+import json
+
+# read config json file
+config = open('./config.json', 'r+')
+config_obj = json.load(config)
+
+# tokens
+CLIENT_ID = config_obj.get("client_id")
+SECRET_TOKEN = config_obj.get("secret_token")
+USER_AGENT = config_obj.get("user_agent")
